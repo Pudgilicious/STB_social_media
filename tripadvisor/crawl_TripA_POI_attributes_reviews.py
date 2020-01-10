4d16d00e222c86d6717ce3c6e8a7c9bd2212af1b
zyf0717 <zyf0717@gmail.com>
2020-01-10 18:04:31 +0800

    TripAdvisor Crawler WIP

--- tripadvisor/crawl_TripA_POI_attributes_reviews.py
+++ tripadvisor/crawl_TripA_POI_attributes_reviews.py
@@ -1,1 +1,30 @@
+import pandas as pd
+import mysql.connector
+import yaml
+import utils
+from selenium import webdriver
+from datetime import datetime
+from time import sleep
 
+cnx = mysql.connector.connect(host='localhost',
+                              database='sentimentDB',
+                              user='test_user',
+                              password='Password123!')
+    
+class crawl_trip_advisor:
+    def __init__(self, cnx):
+        cursor = cnx.cursor()
+        self.poi_list = 
+        self.reviews = 
+    
+    def crawl_and_add(self):
+    
+    
+    def crawl_attributes(self):
+     
+    
+    def crawl_reviews(self):
+        
+        
+    def add_df_reviews(self, reviews):
+        
