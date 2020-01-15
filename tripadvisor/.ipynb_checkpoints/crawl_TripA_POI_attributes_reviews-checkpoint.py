c21f13865fa2dab9c497076d6197967a232a7289
zyf0717 <zyf0717@gmail.com>
2020-01-15 15:31:51 +0800

    TripAdvisor updates

--- tripadvisor/.ipynb_checkpoints/crawl_TripA_POI_attributes_reviews-checkpoint.py
+++ tripadvisor/.ipynb_checkpoints/crawl_TripA_POI_attributes_reviews-checkpoint.py
@@ -1,27 +1,32 @@
 import pandas as pd
 import mysql.connector
+import re
 import yaml
 import utils
 from selenium import webdriver
 from datetime import datetime
 from time import sleep
 
-cnx = mysql.connector.connect(host='localhost',
-                              database='sentimentDB',
-                              user='test_user',
-                              password='Password123!')
+with open('config_file.yml') as file:
+    configs = yaml.load(file, Loader=yaml.FullLoader)
+
+cnx = mysql.connector.connect(host=configs['host'],
+                              database=configs['database'],
+                              user=configs['user'],
+                              password=configs['password'])
     
 class crawl_trip_advisor:
     def __init__(self, cnx):
         cursor = cnx.cursor()
         self.poi_list = 
         self.reviews = 
+        self.driver = webdriver.Chrome('./chromedriver')
     
     def crawl_and_add(self):
-    
+        
     
     def crawl_attributes(self):
-     
+
     
     def crawl_reviews(self):
         
