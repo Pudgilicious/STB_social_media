74f0437b10a06f50e211aae6fe7d5a99bcc819ce
zyf0717 <zyf0717@gmail.com>
2020-01-29 16:17:40 +0800

    Code for CSV input

--- tripadvisor/main.py
+++ tripadvisor/main.py
@@ -9,40 +9,15 @@ with open('config_file.yml') as file:
     configs = yaml.load(file, Loader=yaml.FullLoader)
 
 chromedriver_path = configs['General']['chromedriver_path']
+csv_input_path = configs['TripAdvisor']['csv_input_path']
 
 db_in_flag = configs['TripAdvisor']['read_from_database']
 db_out_flag = configs['TripAdvisor']['write_to_database']
 
 if db_in_flag == 'csv':
-
-    ### FOR POC ONLY ###
-    poi_index = [4,
-                 5,
-                 6
-                 #183,
-                 #287,
-                 #310
-                ]
-    poi_name = ['National Orchid Garden',
-                'Cloud Forest',
-                'Marina Bay Sands Skypark'
-                #'Kent Ridge Park',
-                #'Clementi Mall',
-                #'Ann Siang Hill Park'
-               ]
-    poi_url = ['https://www.tripadvisor.com.sg/Attraction_Review-g294265-d317453-Reviews-National_Orchid_Garden-Singapore.html',
-               'https://www.tripadvisor.com.sg/Attraction_Review-g294265-d4400781-Reviews-Cloud_Forest-Singapore.html',
-               'https://www.tripadvisor.com.sg/Attraction_Review-g294265-d1837767-Reviews-Marina_Bay_Sands_Skypark-Singapore.html'
-               #'https://www.tripadvisor.com.sg/Attraction_Review-g294265-d2291384-Reviews-Kent_Ridge_Park-Singapore.html',
-               #'https://www.tripadvisor.com.sg/Attraction_Review-g294265-d12309383-Reviews-Clementi_Mall-Singapore.html',
-               #'https://www.tripadvisor.com.sg/Attraction_Review-g294265-d8484024-Reviews-Ann_Siang_Hill_Park-Singapore.html'
-              ]
-    poi_df = pd.DataFrame(
-        dict(poi_index=poi_index,
-             poi_name=poi_name,
-             poi_url=poi_url)
-            )
-    ####################
+    poi_df = pd.read_csv(csv_input_path)
+    poi_df['URL'] = poi_df['URL'].apply(lambda x: 'https://tripadvisor.com.sg' + x)
+    poi_df = poi_df[8:]
 
 if db_in_flag != 'csv':
     poi_df = pd.DataFrame()
