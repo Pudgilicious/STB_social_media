de81440337759d89faf69bc9d1abe74b2645089b
jhuang433 <jhuang433@gatech.edu>
2020-01-30 11:37:37 +0800

    modified files

--- ctrip/ctrip_r/8_prep_ctrip_agg_list.R
+++ ctrip/ctrip_r/8_prep_ctrip_agg_list.R
@@ -1,6 +1,6 @@
 library(tidyverse)
 
-df2 = read_csv("./20200117_chinese_ctrip_list.csv")
+df2 = read_csv("./ctrip/ctrip_r/20200117_chinese_ctrip_list.csv")
 df2$X1 = 1:nrow(df2)
 df2 = df2 %>%
         rename(., id = X1) %>%
@@ -13,7 +13,7 @@ df2$id = 1:nrow(df2)
 #To display chinese character
 stringi::stri_trans_general(df2$POI_chinese_name, "zh")
 
-df2_1 = read_csv("./20200117_eng_ctrip_list.csv")
+df2_1 = read_csv("./ctrip/ctrip_r/20200117_eng_ctrip_list.csv")
 df2_1 = df2_1[!duplicated(df2_1[c(2,3)]), ]
 df2_1 = df2_1 %>%
           dplyr::rename(id_chinese = X1)
@@ -35,7 +35,7 @@ df2 = df2[, -which(names(df2) == "id_chinese")]
 df2$WEBSITE_INDEX = 2
 
 names(df2) = c("POI_INDEX", "POI_CHINESE_NAME", "URL", "TOTAL_REVIEWS", "POI_NAME", "WEBSITE_INDEX")
-filename = paste0(gsub("-","", Sys.Date()), "_TB_SOCIAL_CTRIP_CRAWL_LIST.csv")
+filename = paste0("ctrip/ctrip_r/",gsub("-","", Sys.Date()), "_TB_SOCIAL_CTRIP_CRAWL_LIST.csv")
 
 write.csv(df2, filename, row.names = F)
 
