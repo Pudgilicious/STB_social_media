7959a22f37479c9d4af3df1ac7a36c4595406376
zyf0717 <zyf0717@gmail.com>
2020-02-06 17:52:34 +0800

    Sentiment scorer FSM complete

--- tripadvisor/score.py
+++ tripadvisor/score.py
@@ -1,14 +1,17 @@
 import yaml
 from ibm_watson import NaturalLanguageUnderstandingV1
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from time import sleep
+
 from sentiment_scorer import SentimentScorer
 
 with open('config_file.yml') as file:
     configs = yaml.load(file, Loader=yaml.FullLoader)
 
 with open('./tripadvisor/IBM_api_keys.yml') as file:
-    api_keys = yaml.load(file, Loader=yaml.FullLoader)
+    api_keys = yaml.load(file, Loader=yaml.FullLoader)['API_Keys']
 
+target_folder = '200126_094431'
 target_folder = configs['TripAdvisor']['target_folder']
 
 
@@ -17,9 +20,10 @@ class SentimentScorerFSM:
         self.sentiment_scorer = SentimentScorer(target_folder)
         self.authenticator = None
         self.nlu = None
+        self.api_key_index = None
 
     def initialise_nlu(self):
-        self.authenticator = IAMAuthenticator(api_keys[0])
+        self.authenticator = IAMAuthenticator(api_keys[self.api_key_index])
         self.nlu = NaturalLanguageUnderstandingV1(
             version='2019-07-12',
             authenticator=self.authenticator
@@ -28,10 +32,17 @@ class SentimentScorerFSM:
             'https://gateway.watsonplatform.net/natural-language-understanding/api/v1/analyze?version=2019-07-12')
 
     def start(self):
-        self.initialise_nlu()
-        self.sentiment_scorer.score_sentiments(nlu)
+        self.api_key_index = 0
+        while self.sentiment_scorer.fsm_state != 4:
+            self.initialise_nlu()
+            self.sentiment_scorer.score_sentiments(self.nlu)
+            if self.sentiment_scorer.fsm_state == 2:
+                self.api_key_index += 1
+                sleep(2)
+            if self.sentiment_scorer.fsm_state == 3:
+                sleep(2)
+                continue
 
 
-if __name__=="__score__":
-    fsm = SentimentScorerFSM(target_folder)
-    fsm.start()
+fsm = SentimentScorerFSM(target_folder)
+fsm.start()
--- tripadvisor/sentiment_scorer.py
+++ tripadvisor/sentiment_scorer.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import traceback
+import os
 from datetime import datetime
-from os import listdir
 from pandas.io.json import json_normalize
 from ibm_watson.natural_language_understanding_v1 import Features, EntitiesOptions, KeywordsOptions
 
@@ -54,10 +54,16 @@ class SentimentScorer:
 
     def __init__(self, target_folder):
         self.target_folder = target_folder
-        self.csv_list = listdir('./finalised_output/{}/reviews'.format(target_folder))
+        self.csv_list = os.listdir('./tripadvisor/finalised_output/{}/reviews'.format(target_folder))
         self.poi_list = sorted([int(i[:i.find('.csv')]) for i in self.csv_list])  # List of POI indexes from file names
         self.keywords_col_names_df = pd.DataFrame(columns=self.keywords_col_names)
         self.entities_col_names_df = pd.DataFrame(columns=self.entities_col_names)
+
+        self.sentiment_folder_path = './tripadvisor/finalised_output/{}/sentiments_{}/'.format(
+            self.target_folder,
+            datetime.now().strftime('%y%m%d_%H%M%S')
+        )
+        os.makedirs(self.sentiment_folder_path)
 
         self.nlu = None
         self.fsm_state = 0
@@ -79,79 +85,86 @@ class SentimentScorer:
         self.entities_df = None
 
     def score_sentiments(self, nlu):
-        self.fsm_state = 1
         self.nlu = nlu
         while self.poi_list:
-            self.datetime_string = datetime.now().strftime('%y%m%d_%H%M%S')
-            self.current_poi_index = self.poi_list.pop(0)
-            self.keywords_file_path = './tripadvisor/finalised_output/{}/reviews/{}_keywords_{}.csv'.format(
-                self.target_folder,
-                self.current_poi_index,
-                self.datetime_string
-            )
-            self.entities_file_path = './tripadvisor/finalised_output/{}/reviews/{}_entities_{}.csv'.format(
-                self.target_folder,
-                self.current_poi_index,
-                self.datetime_string
-            )
 
-            # Create empty CSVs with all headers
-            keywords_col_names_df = pd.DataFrame(columns=self.keywords_col_names)
-            keywords_col_names_df.to_csv(
-                self.keywords_file_path,
-                mode='a',
-                index=False
-            )
-            entities_col_names_df = pd.DataFrame(columns=self.entities_col_names)
-            entities_col_names_df.to_csv(
-                self.entities_file_path,
-                mode='a',
-                index=False
-            )
+            # Only create the below if FSM state is not 2 or 3
+            if self.fsm_state != 2 and self.fsm_state != 3:
+                self.datetime_string = datetime.now().strftime('%y%m%d_%H%M%S')
+                self.current_poi_index = self.poi_list.pop(0)
+                self.keywords_file_path = self.sentiment_folder_path + '{}_keywords_{}.csv'.format(
+                    self.current_poi_index,
+                    self.datetime_string
+                )
+                self.entities_file_path = self.sentiment_folder_path + '{}_entities_{}.csv'.format(
+                    self.current_poi_index,
+                    self.datetime_string
+                )
+
+                # Create empty CSVs with all headers
+                keywords_col_names_df = pd.DataFrame(columns=self.keywords_col_names)
+                keywords_col_names_df.to_csv(
+                    self.keywords_file_path,
+                    mode='a',
+                    index=False
+                )
+                entities_col_names_df = pd.DataFrame(columns=self.entities_col_names)
+                entities_col_names_df.to_csv(
+                    self.entities_file_path,
+                    mode='a',
+                    index=False
+                )
 
             # Scoring
             self.score_sentiments_1_poi()
 
+            # Return if error has occured (FSM state is 2 or 3)
+            if self.fsm_state != 1:
+                return
+
+        self.fsm_state = 4
+
     def score_sentiments_1_poi(self):
-        self.current_reviews_df = pd.read_csv('./tripadvisor/finalised_output/{}/reviews/{}.csv'.format(
-            self.target_folder,
-            self.current_poi_index
-        ))
+        self.fsm_state = 1
+        if self.current_reviews_df is None or len(self.current_reviews_df.index) == 0:
+            self.current_reviews_df = pd.read_csv('./tripadvisor/finalised_output/{}/reviews/{}.csv'.format(
+                self.target_folder,
+                self.current_poi_index
+            ))
 
         while len(self.current_reviews_df.index) > 0:
             self.current_df_row = self.current_reviews_df.iloc[0]
             self.current_review_id = self.current_df_row['REVIEW_ID']
             self.current_review_text = self.current_df_row['REVIEW_BODY']
+            print(self.current_review_id)
 
             try:
                 self.get_api_response()
             except:
                 self.fsm_state = 2
                 print("Error in API call. Please see log file.")
-                log = open(
-                    './tripadvisor/finalised_output/{}/reviews/log.txt'.format(self.target_folder),
-                    'a+')
+                log = open(self.sentiment_folder_path + 'log.txt', 'a+')
                 log.write('POI index: {}, row number: {}, {}\n'.format(
                     self.current_poi_index,
                     self.current_row_number,
                     datetime.now()))
                 log.write(traceback.format_exc() + '\n')
                 log.close()
+                return
 
             try:
                 self.parse_response()
             except:
                 self.fsm_state = 3
                 print("Error in parsing API response. Please see log file.")
-                log = open(
-                    './tripadvisor/finalised_output/{}/reviews/log.txt'.format(self.target_folder),
-                    'a+')
+                log = open(self.sentiment_folder_path + 'log.txt', 'a+')
                 log.write('POI index: {}, row number: {}, {}\n'.format(
                     self.current_poi_index,
                     self.current_row_number,
                     datetime.now()))
                 log.write(traceback.format_exc() + '\n')
                 log.close()
+                return
 
             self.write_to_csv()
             self.current_df_row = None
@@ -176,7 +189,7 @@ class SentimentScorer:
         if 'keywords' in self.current_api_response.keys():
             self.keywords_df = json_normalize(self.current_api_response['keywords'])\
                 .rename(columns=self.keywords_col_names_map)
-            self.keywords_df.insert(0, 'REVIEW_ID', self.keywords_df.iloc[0]['REVIEW_ID'])
+            self.keywords_df.insert(0, 'REVIEW_ID', self.current_review_id)
             self.keywords_df.insert(0, 'WEBSITE_ID', 1)
             self.keywords_df = self.keywords_col_names_df.append(self.keywords_df, sort=False)
 
@@ -184,7 +197,7 @@ class SentimentScorer:
             self.entities_df = json_normalize(self.current_api_response['entities'])\
                 .rename(columns=self.entities_col_names_map)
             if self.entities_df.shape[1] != 0:
-                self.entities_df.insert(0, 'REVIEW_ID', self.entities_df.iloc[0]['REVIEW_ID'])
+                self.entities_df.insert(0, 'REVIEW_ID', self.current_review_id)
                 self.entities_df.insert(0, 'WEBSITE_ID', 1)
                 self.entities_df = self.entities_col_names_df.append(self.entities_df, sort=False)
 
@@ -195,9 +208,10 @@ class SentimentScorer:
             header=False,
             index=False
         )
-        self.entities_df.to_csv(
-            self.entities_file_path,
-            mode='a',
-            header=False,
-            index=False
-        )
+        if self.entities_df.shape[1] != 0:
+            self.entities_df.to_csv(
+                self.entities_file_path,
+                mode='a',
+                header=False,
+                index=False
+            )
