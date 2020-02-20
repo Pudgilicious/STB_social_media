897d01ee4aa971f6bc5eb8598f25bc81f0d5eef3
zyf0717 <zyf0717@gmail.com>
2020-02-20 22:46:03 +0800

    Word clouds for TripA

---
+++ tripadvisor/.ipynb_checkpoints/score-checkpoint.py
@@ -0,0 +1,28 @@
+import sys
+sys.path.append('./common_class_functions')
+
+import yaml
+from ibm_sentiment_scorer_fsm import IBMSentimentScorerFSM
+
+
+with open('config_file.yml') as file:
+    configs = yaml.load(file, Loader=yaml.FullLoader)
+
+# Amend the corresponding variables in config_file.yml to continue from previous API calls.
+target_folder = configs['TripAdvisor']['target_folder']
+continue_in_folder = configs['TripAdvisor']['continue_in_folder']
+continue_from_poi_index = configs['TripAdvisor']['continue_from_poi_index']
+continue_from_row_index = configs['TripAdvisor']['continue_from_row_index']
+
+# Initialize FSM object
+fsm = IBMSentimentScorerFSM(
+    website_name='tripadvisor',
+    website_id=1,
+    target_folder=target_folder,
+    continue_in_folder=continue_in_folder,
+    continue_from_poi_index=continue_from_poi_index,
+    continue_from_row_index=continue_from_row_index
+)
+
+# Start API calls
+fsm.start()
