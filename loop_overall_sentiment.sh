05d34bf6f800c1d8a744526451a4c16a9e6972a9
jia <xin96jia@gmail.com>
2020-03-11 17:16:52 +0800

    loop all overall sentiment

---
+++ loop_overall_sentiment.sh
@@ -0,0 +1,21 @@
+cp /home/stb/git/STB_social_media_analytics/config_files/200128_111646/config_file.yml /home/stb/git/STB_social_media_analytics
+python ./tripadvisor/score.py
+cp /home/stb/git/STB_social_media_analytics/config_files/200129_102527/config_file.yml /home/stb/git/STB_social_media_analytics
+python ./tripadvisor/score.py
+cp /home/stb/git/STB_social_media_analytics/config_files/200129_172433/config_file.yml /home/stb/git/STB_social_media_analytics
+python ./tripadvisor/score.py
+cp /home/stb/git/STB_social_media_analytics/config_files/200130_091345/config_file.yml /home/stb/git/STB_social_media_analytics
+python ./tripadvisor/score.py
+cp /home/stb/git/STB_social_media_analytics/config_files/200130_102635/config_file.yml /home/stb/git/STB_social_media_analytics
+python ./tripadvisor/score.py
+cp /home/stb/git/STB_social_media_analytics/config_files/200130_113340/config_file.yml /home/stb/git/STB_social_media_analytics
+python ./tripadvisor/score.py
+cp /home/stb/git/STB_social_media_analytics/config_files/200130_135426/config_file.yml /home/stb/git/STB_social_media_analytics
+python ./tripadvisor/score.py
+cp /home/stb/git/STB_social_media_analytics/config_files/200131_090828/config_file.yml /home/stb/git/STB_social_media_analytics
+python ./tripadvisor/score.py
+cp /home/stb/git/STB_social_media_analytics/config_files/200131_102045/config_file.yml /home/stb/git/STB_social_media_analytics
+python ./tripadvisor/score.py
+cp /home/stb/git/STB_social_media_analytics/config_files/200131_161919/config_file.yml /home/stb/git/STB_social_media_analytics
+python ./tripadvisor/score.py
+
